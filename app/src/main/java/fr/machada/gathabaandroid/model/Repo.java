9265da30558372d8ef88f54e69d8ee49dc9258cc
macha <macha.dacosta@gmail.com>
2015-11-24 14:56:48 +0100

    Add Repo object

---
+++ app/src/main/java/fr/machada/gathabaandroid/model/Repo.java
@@ -0,0 +1,21 @@
+package fr.machada.gathabaandroid.model;
+
+/**
+ * Created by macha on 24/11/15.
+ */
+public class Repo {
+
+    String name;
+
+    public Repo(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
