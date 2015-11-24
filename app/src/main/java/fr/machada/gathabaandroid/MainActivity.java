e7fc80410033e3a1e12ac2f417ec745404e21e5f
macha <macha.dacosta@gmail.com>
2015-11-24 15:20:09 +0100

    Execute get request

--- app/src/main/java/fr/machada/gathabaandroid/MainActivity.java
+++ app/src/main/java/fr/machada/gathabaandroid/MainActivity.java
@@ -5,23 +5,29 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import java.util.List;
 
 import fr.machada.gathabaandroid.model.Repo;
 import fr.machada.gathabaandroid.service.GitHubService;
 import retrofit.Call;
+import retrofit.Callback;
 import retrofit.GsonConverterFactory;
+import retrofit.Response;
 import retrofit.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String API_BASE = "https://api.github.com";
+    private static final String TAG = "MAIN ACTIVITY";
     Retrofit mRetrofit;
     GitHubService mService;
+    Call<List<Repo>> mRepos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,12 +47,32 @@ public class MainActivity extends AppCompatActivity {
 
         prepareRequest();
         constructRequest();
+        executeRequest();
+    }
+
+    private void executeRequest() {
+        mRepos.enqueue(new Callback<List<Repo>>() {
+
+            @Override
+            public void onResponse(Response<List<Repo>> response, Retrofit retrofit) {
+                Toast.makeText(getApplicationContext(), String.format("OK"), Toast.LENGTH_SHORT).show();
+                List<Repo> allRepos = response.body();
+                for (int i = 0; i < allRepos.size(); i++)
+                    Log.i(TAG, String.format(" Un repo : %s", allRepos.get(i)));
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                Toast.makeText(getApplicationContext(), String.format("KO"), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private void constructRequest() {
-        Call<List<Repo>> repos = mService.listRepos("machadacosta");
+        mRepos = mService.listRepos("machadacosta");
 
     }
+
 
     private void prepareRequest() {
         mRetrofit = new Retrofit.Builder()
