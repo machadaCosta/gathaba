a72994c587fbee3e991a74419a97e5d4cf715b33
macha <macha.dacosta@gmail.com>
2015-11-30 17:45:52 +0100

    Convert Repo to RealmObject

--- app/src/main/java/fr/machada/gathabaandroid/model/Repo.java
+++ app/src/main/java/fr/machada/gathabaandroid/model/Repo.java
@@ -1,10 +1,15 @@
 package fr.machada.gathabaandroid.model;
 
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
 /**
  * Created by macha on 24/11/15.
  */
-public class Repo {
+public class Repo extends RealmObject {
 
+    @PrimaryKey
+    int id;
     String name;
 
     public Repo(String name) {
