b800edcf6c03251709f9fc33cf3274e24e7ffa82
macha <macha.dacosta@gmail.com>
2015-12-03 17:43:15 +0100

    Add getter & setter for id field

--- app/src/main/java/fr/machada/gathabaandroid/model/Repo.java
+++ app/src/main/java/fr/machada/gathabaandroid/model/Repo.java
@@ -11,12 +11,20 @@ public class Repo extends RealmObject {
     @PrimaryKey
     private int id;
     private String name;
-    
+
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }
